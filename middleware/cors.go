268e4c7980a8cdd63e04c5b72dc0216b847b5776
xiaoping.wang <wangxp0711@hotmail.com>
2021-01-23 22:59:34 +0800

    解决跨域配置问题

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,23 +1,26 @@
 package middleware
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
-// func Cors() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		cors.New(cors.Config{
-// 			AllowOrigins:     []string{"*"},
-// 			AllowMethods:     []string{"*"},
-// 			AllowHeaders:     []string{"Origin"},
-// 			ExposeHeaders:    []string{"Content-Length", "Authorization"},
-// 			AllowCredentials: true,
-// 			MaxAge:           12 * time.Hour,
-// 		})
-// 	}
-// }
-
 func Cors() gin.HandlerFunc {
-	return cors.Default()
+	return cors.New(
+		cors.Config{
+			AllowOrigins:     []string{"*"},
+			AllowMethods:     []string{"*"},
+			AllowHeaders:     []string{"Origin"},
+			ExposeHeaders:    []string{"Content-Length", "Authorization", "Content-Type"},
+			AllowCredentials: true,
+			MaxAge:           12 * time.Hour,
+		},
+	)
 }
+
+// 默认 cors 跨域配置
+// func Cors() gin.HandlerFunc {
+// 	return cors.Default()
+// }
