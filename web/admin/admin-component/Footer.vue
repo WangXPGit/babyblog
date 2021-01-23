3b0fbbf034e4a3831f07167148e3dcec0f5219b3
xiaoping.wang <wangxp0711@hotmail.com>
2021-01-24 00:16:44 +0800

    管理后台页面基本框架搭建

---
+++ web/admin/admin-component/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="footer">
+        <span> ---- baby blog admin --- </span>
+    </div>
+</template>
+
+<style scoped>
+.footer {
+    background-color: #eee;
+    text-align: center;
+}
+.footer.span {
+    font-size: 10px;
+}
+</style>
--- web/admin/src/views/Admin.vue
+++ web/admin/src/views/Admin.vue
@@ -1,5 +1,56 @@
 <template>
-    <div>
-        管理页面
-    </div>
+    <a-layout class="container">
+      <a-layout-sider>
+          <div class="log">
+              <span>Baby Blog</span>
+          </div>
+          <Nav></Nav>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header class="headerBtn">
+            <a-button type="danger" @click="logout">退出</a-button>
+        </a-layout-header>
+        <a-layout-content>
+            <router-view></router-view>
+        </a-layout-content>
+        <a-layout-footer>
+            <Footer></Footer>
+        </a-layout-footer>
+      </a-layout>
+    </a-layout>
 </template>
+
+<script>
+import Nav from '../../admin-component/Nav'
+import Footer from '../../admin-component/Footer'
+export default {
+    components : {Nav, Footer},
+    methods: {
+        logout() {
+            sessionStorage.clear('token')
+            this.$router. push('/login')
+        }
+    }
+}
+</script>
+<style scoped>
+.container {
+    height: 100%;
+}
+
+.headerBtn {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+
+.log {
+    height: 32px;
+    margin: 16px;
+    background: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 17px;
+}
+</style>
